005ed6b9ad4675496ff8ff5987049200d946e967
Surya Fotedar <suryafotedar@gmail.com>
2019-07-09 10:50:55 -0500

    add cors

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ||  3000;
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client/dist'));
